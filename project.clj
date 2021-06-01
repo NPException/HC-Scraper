438a773b538805bf0333b7693ff11ed573601f81
NPException <npexception42@gmail.com>
2021-06-01 18:04:14 +0200

    switch project to MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject hc-scraper "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
